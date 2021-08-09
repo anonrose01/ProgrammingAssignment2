6d3a76cd773c9c4897005b57d06f80b5a25229ce
anonrose01 <87739649+anonrose01@users.noreply.github.com>
2021-08-09 00:46:12 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,6 @@
+## This function just puts in x as an input if it is a matrix
+## and if not, as a NULL 
+
 makeCacheMatrix <- function(x = matrix()) {
   inv<-NULL
   set<-function(y) {
@@ -13,6 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setInv=setInv,
        getInv=getInv)
 }
+
+## This gives you the inversed matrix and caches it so you can retrieve
+## it again later
 
 cacheSolve <- function(x, ...) {
   inv<-x$getInv()
